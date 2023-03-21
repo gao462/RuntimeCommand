39fa2e12a1f4f1e2c91aaf64d2f6174c06ed7979
Jianfei Gao <gao462@purdue.edu>
2023-03-21 18:20:02 -0400

    update.

--- initialize.sh
+++ initialize.sh
@@ -2,10 +2,12 @@
 set -e
 
 #
-for directory in Library Download Cached Dataset Coding Coded; do
+for directory in Library Download Cached Dataset; do
     #
     mkdir -p ${directory}
 done
+mkdir -p Studio/Coding
+mkdir -p Studio/Coded
 
 #
 rm -f .bashrc-home
